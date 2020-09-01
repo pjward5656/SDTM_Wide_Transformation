04ba274dc78aa4f734d42da1567ab1926d1fe0db
Patrick Ward <pjward@ad.unc.edu>
2020-09-01 10:20:25 -0400

    Created code transforming SDTM to wide format

---
+++ Transform.R
@@ -0,0 +1,33 @@
+#Program that converts QS SDTM data into wide format
+
+library(tidyverse)
+library(haven)
+
+#Read in the QS data and metadata
+qs<-read_sas("J:/BACPAC/SC/SASdata/Simulated/200706/QSmd.sas7bdat")
+meta<-read_sas("J:/BACPAC/SC/Review/BP0001/200706/qsmd_metadata.sas7bdat")
+
+
+#Lets write this as normal code first, then convert into a generalizable function
+wide<-qs %>% 
+  #Selecting the variables I believe I need
+  select(USUBJID, VISIT, QSDTC, QSTESTCD, QSSTRESC) %>% 
+  pivot_wider(id_cols=c(USUBJID, VISIT, QSDTC),
+              names_from=QSTESTCD,
+              values_from=QSSTRESC)
+
+#As a function
+transform_wide<-function(x){
+  x %>% 
+    select(USUBJID, VISIT, QSDTC, QSTESTCD, QSSTRESC) %>% 
+    pivot_wider(id_cols=c(USUBJID, VISIT, QSDTC),
+                names_from=QSTESTCD,
+                values_from=QSSTRESC)
+}
+
+#Example use
+wide_new<-qs %>% 
+  transform_wide()
+
+#Export result
+write_csv(wide_new, "J:/BACPAC/Users/WardPatrick/SDTM_Wide_Transformation/qs_sim_wide.csv")
