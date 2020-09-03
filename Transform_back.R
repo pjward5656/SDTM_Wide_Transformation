0f328ac95ca090e689d29a3701f64b6111b28aea
Patrick Ward <patrickjward@uky.edu>
2020-09-03 13:03:44 -0400

    First step of determining how to work with derived variables

--- Transform_back.R
+++ Transform_back.R
@@ -4,14 +4,25 @@ library(tidyverse)
 library(haven)
 library(lubridate)
 
-#Read in the wide QS data and metadata, as well as the original QS data for comparison purposes
+#Read in the wide QS data and metadata, as well as the original QS data for comparison purposes. Also need the DM data for QSDY calculation
 meta<-read_sas("J:/BACPAC/SC/Review/BP0001/200706/qsmd_metadata.sas7bdat")
 wide<-read_csv("J:/BACPAC/Users/WardPatrick/SDTM_Wide_Transformation/qs_sim_wide.csv", col_types=cols(.default = "c"))
 qs<-read_sas("J:/BACPAC/SC/SASdata/Simulated/200706/QSmd.sas7bdat")
+dm<-read_sas("J:/BACPAC/SC/SASdata/Simulated/200706/DM.sas7bdat")
 
-#First step is making it long
+#First, need to create the derived variables while the data is still in wide format
+#Identify derived variables to create
+#Create a df of all the derived variables that need to be created
+derived<-meta %>% 
+  filter(DERIVED_FLAG=="Y")
+vars<-derived$TESTCODE_VALUE
+
+#So, the derived variables are derived from QSTRESN, not QSTRESC. This is problematic based on how I created the wide dataset.
+
+
+#Then, make the data long
 long<-wide %>% 
-  pivot_longer(cols=c(-USUBJID, -VISIT, -QSDTC, -QSDY),
+  pivot_longer(cols=c(-USUBJID, -VISIT, -QSDTC),
                names_to="QSTESTCD",
                values_to="QSSTRESC")
 
@@ -28,15 +39,23 @@ qstestcd_lu<-meta %>%
 qsstresc_lu<-meta %>% 
   select(QSTESTCD=TESTCODE_VALUE,
          QSSTRESC=STRESC_TERM,
-         QSSTRESN=STRESN_TERM,
-         QSDRVFL=DERIVED_FLAG) %>% 
+         QSSTRESN=STRESN_TERM) %>% 
   distinct()
+
+#Create a df with the reference start day to calculate QSDY
+start_date<-dm %>% 
+  select(USUBJID, RFSTDTC)
 
 #Create vector of column names of the QS data
 order<-colnames(qs)
 df_order<-as.data.frame(order)
 
 #Now, we have to add all the associated information for all of the questions
+
+
+
+
+#I need to re-think the below.
 qs_<-long %>% 
   mutate(STUDYID=str_split(USUBJID, "-", simplify=TRUE)[1],
          VISITNUM=str_split(VISIT, " ", simplify=TRUE)[,2],
@@ -44,8 +63,11 @@ qs_<-long %>%
   #Need to merge in QSCAT, QSSCAT, QSTEST, and QSEVLNT based on QSTESTCD
   left_join(qstestcd_lu) %>% 
   arrange(USUBJID, VISIT) %>% 
-  #Need to merge in QSSTRESN and QSDRVFL based upon QSTESTCD and QSSTRESC
+  #Need to merge in QSSTRESN based upon QSTESTCD and QSSTRESC
   left_join(qsstresc_lu) %>% 
+  #Calculate the study day
+  left_join(start_date) %>% 
+  #Calculate derived variables
   #Arrange by needed variables and create the QSSEQ variable
   arrange(STUDYID, USUBJID, QSCAT, QSSCAT, VISITNUM, QSTESTCD) %>% 
   group_by(USUBJID) %>% 
